6c0791e84fbcbb83499f2f67eb4ab869893d4e40
pragyak765 <pragyakhadka765@gmail.com>
2024-07-20 12:56:54 +0545

    admin and database

--- travello/admin.py
+++ travello/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from  .models import Destination
-from .models import Detailed_desc
+from  .models import Destination,Detailed_desc, pessanger_detail, Cards, NetBanking, Transactions
 # Register your models here.
 
 admin.site.register(Destination)
 admin.site.register(Detailed_desc)
+admin.site.register(pessanger_detail)
+admin.site.register(Cards)
+admin.site.register(NetBanking)
+admin.site.register(Transactions)
 
